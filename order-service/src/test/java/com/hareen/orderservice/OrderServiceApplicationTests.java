bea7a27c81598c08bbb4fbc99aa62c707dda70a5
iamHareen <helanm06@gmail.com>
2025-11-11 04:23:23 +0530

    Add integration test with WireMock for inventory check

--- order-service/src/test/java/com/hareen/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/hareen/orderservice/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.hareen.orderservice;
 
+import com.hareen.orderservice.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,12 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
     @ServiceConnection
@@ -39,6 +42,8 @@ class OrderServiceApplicationTests {
                     "quantity": 1
                 }
                 """;
+
+        InventoryClientStub.stubInventoryCall("iphone_15", 1);
 
         var responseBodyString = RestAssured.given()
                 .contentType("application/json")
